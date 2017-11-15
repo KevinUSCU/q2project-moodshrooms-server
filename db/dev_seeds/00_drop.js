07e9faa3c9fa4b3a75736d0664e17f2a62a3f375
Kevin Springer <KevinUSCU@outlook.com>
2017-11-15 02:13:42 -0800

    Fix drop sequence error.

--- db/dev_seeds/00_drop.js
+++ db/dev_seeds/00_drop.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries in reverse FK order
   return knex('shared_shrooms').del()
-  return knex('shrooms').del()
-  return knex('users').del()
-  return knex('shroom_parts').del()
+    .then(() => knex('shrooms').del())
+    .then(() => knex('users').del())
+    .then(() => knex('shroom_parts').del())
 }
