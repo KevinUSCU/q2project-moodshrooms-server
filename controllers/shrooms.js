5ea38e5162b82de8c2a915ccfe5b603131d4374a
Kevin Springer <KevinUSCU@outlook.com>
2017-11-16 01:38:56 -0800

    Remove merging of shroom and parts on the server. This is now a client-side operation.

--- controllers/shrooms.js
+++ controllers/shrooms.js
@@ -2,44 +2,16 @@ const { Shroom, Part } = require('../models')
 
 function get (req, res, next) {
   Shroom.get()
-    .then(shrooms => {
-      let promises = shrooms.map(shroom => attachParts(shroom))
-      Promise.all(promises)
-        .then(result => res.json(result))
-    })
+  .then(shrooms => res.json(shrooms))
 }
 
 function getShroom (req, res, next) {
   const id = req.params.id
   Shroom.getShroom(id)
-    .then(shroom => {
-      if (!shroom) return next({ status: 404, error: `Error 404: That's not a valid moodshroom!` })
-      attachParts(shroom)
-        .then(result => res.json(result))
-    })
-}
-
-function attachParts(shroom) {
-  let promises = [
-    Part.getPart(shroom.cap),
-    Part.getPart(shroom.base),
-    Part.getPart(shroom.mouth),
-    Part.getPart(shroom.eyes),
-    Part.getPart(shroom.eyeballs),
-    Part.getPart(shroom.eyebrows),
-    Part.getPart(shroom.flourish)
-  ]
-  return Promise.all(promises)
-    .then(result => {
-      shroom.cap = result[0].path
-      shroom.base = result[1].path
-      shroom.mouth = result[2].path
-      shroom.eyes = result[3].path
-      shroom.eyeballs = result[4].path
-      shroom.eyebrows = result[5].path
-      shroom.flourish = result[6].path
-      return shroom
-    })
+  .then(shroom => {
+    if (!shroom) return next({ status: 404, error: `Error 404: That's not a valid moodshroom!` })
+    return res.json(shroom)
+  })
 }
 
 function createShroom(req, res, next) {
