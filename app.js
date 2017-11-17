2babad5f9b6fd5b78164dd10fe489fb6dc97d924
Kevin Springer <KevinUSCU@outlook.com>
2017-11-16 16:09:46 -0800

    Debugging cors.

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ if (environment === 'development') {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
   }
 }
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
+app.use(cors())
 
 // Set up Body-Parser
 const bodyParser = require('body-parser')
