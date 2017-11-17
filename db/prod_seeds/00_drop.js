a0bcc7aa9dcf7019786aa0497e32d3418e5d1f62
Kevin Springer <KevinUSCU@outlook.com>
2017-11-16 17:23:17 -0800

    Update production seeds.

--- db/prod_seeds/00_drop.js
+++ db/prod_seeds/00_drop.js
@@ -1,4 +1,7 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries in reverse FK order
-  return knex('shroom_parts').del()
+  return knex('shared_shrooms').del()
+    .then(() => knex('shrooms').del())
+    .then(() => knex('users').del())
+    .then(() => knex('shroom_parts').del())
 }
